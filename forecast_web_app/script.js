c2e41d179e940477e14a733e63cf5190f14221b3
Jimmy Shultz <jimmyshultz3@gmail.com>
2023-12-16 17:31:41 -0600

    Added a web application that functions similarly to the console application by giving current weather forecast to the user when they search by city and state.

---
+++ forecast_web_app/script.js
@@ -0,0 +1,138 @@
+//document.getElementById('weather-form').addEventListener('submit', handleClick);
+document.getElementById('weather-form').addEventListener('submit', handleSearch);
+
+function handleClick(event) {
+    event.preventDefault();
+    var city = document.getElementById('city').value;
+    if (city == "New York") {
+        getWeather("OKX", 33, 35, city);
+    } else if (city == "Boston") {
+        getWeather("BOX", 71, 90, city);
+    } else if (city == "Orlando") {
+        getWeather("MLB", 26, 68, city);
+    } else if (city == "Chicago") {
+        getWeather("LOT", 76, 73, city);
+    } else if (city == "Washington") {
+        getWeather("LWX", 96, 72, city);
+    } else if (city == "Atlanta") {
+        getWeather("FFC", 51, 87, city);
+    } else if (city == "Seattle") {
+        getWeather("SEW", 125, 68, city);
+    } else if (city == "San Francisco") {
+        getWeather("MTR", 85, 105, city);
+    } else if (city == "Los Angeles") {
+        getWeather("LOX", 155, 45, city);
+    } else if (city == "Current Location") {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                getOfficeId(position.coords.latitude, position.coords.longitude);
+                }, displayGeolocationBlocked());
+        } else {
+            displayGeolocationError();
+        }
+    }
+}
+
+function handleSearch(event) {
+    event.preventDefault();
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    getLatLon(city, state);
+}
+
+function getLatLon(cityName, stateName) {
+    var bingMapsKey = "Ao7gQtkN9LjNj3kHUwHzcACGxSBoqnQ0PFun68SN9kmWirlB41Lzsm7if9S799WL"
+    var url = `http://dev.virtualearth.net/REST/v1/Locations/US/${stateName}/${cityName}/?output=json&key=${bingMapsKey}`
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            getOfficeId(data.resourceSets[0].resources[0].geocodePoints[0].coordinates[0], data.resourceSets[0].resources[0].geocodePoints[0].coordinates[1]);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+}
+
+function getOfficeId(latitude, longitude) {
+    var url = `https://api.weather.gov/points/${latitude},${longitude}`;
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            getWeather(data.properties.gridId, data.properties.gridX, data.properties.gridY, data.properties.relativeLocation.properties.city, data.properties.relativeLocation.properties.state);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function getWeather(office,gridX,gridY,city,state) {
+    var url = 'https://api.weather.gov/gridpoints/' + office + '/' + gridX + "," + gridY + "/forecast";
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayWeather(data, city, state);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function displayWeather(data, city, state) {
+    var weatherInfo = document.getElementById('weather-info');
+    weatherInfo.innerHTML = '';
+    if (state != null) {
+        if (data.type === "Feature") {
+            var period = data.properties.periods[0].name;
+            var temperature = data.properties.periods[0].temperature;
+            var trend = data.properties.periods[0].temperatureTrend;
+            var humidity = data.properties.periods[0].relativeHumidity.value;
+            var windSpeed = data.properties.periods[0].windSpeed;
+            var direction = data.properties.periods[0].windDirection;
+            var forecast = data.properties.periods[0].detailedForecast;
+            var weatherHtml = '<h2>Weather Forecast for ' + period + ' in ' + city + ', ' + state + '</h2>' +
+                '<p>Temperature: ' + temperature + ' &#8451;</p>' +
+                '<p>Humidity: ' + humidity + '%</p>' +
+                '<p>Wind Speed: ' + windSpeed + ' m/s ' + direction + '</p>' +
+                '<h3> Forecast </h3>' +
+                '<p>' + forecast + '</p>';
+            weatherInfo.innerHTML = weatherHtml;
+        } else {
+            weatherInfo.innerHTML = '<p>Failed to retrieve weather information.</p>';
+        }
+    } else {
+        if (data.type === "Feature") {
+            var period = data.properties.periods[0].name;
+            var temperature = data.properties.periods[0].temperature;
+            var trend = data.properties.periods[0].temperatureTrend;
+            var humidity = data.properties.periods[0].relativeHumidity.value;
+            var windSpeed = data.properties.periods[0].windSpeed;
+            var direction = data.properties.periods[0].windDirection;
+            var forecast = data.properties.periods[0].detailedForecast;
+            var weatherHtml = '<h2>Weather Forecast for ' + period + ' in ' + city + '</h2>' +
+                '<p>Temperature: ' + temperature + ' &#8451;</p>' +
+                '<p>Humidity: ' + humidity + '%</p>' +
+                '<p>Wind Speed: ' + windSpeed + ' m/s ' + direction + '</p>' +
+                '<h3> Forecast </h3>' +
+                '<p>' + forecast + '</p>';
+            weatherInfo.innerHTML = weatherHtml;
+        } else {
+            weatherInfo.innerHTML = '<p>Failed to retrieve weather information.</p>';
+        }
+    }
+}
+
+function displayGeolocationBlocked() {
+    var weatherInfo = document.getElementById('weather-info');
+    weatherInfo.innerHTML = 'Please allow geolocation.';
+}
+
+function displayGeolocationError() {
+    var weatherInfo = document.getElementById('weather-info');
+    weatherInfo.innerHTML = 'Geolocation is not supported by this browser.';
+}
